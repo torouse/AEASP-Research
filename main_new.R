641aca6658422851bd05b7f998634ddf576174ab
torouse <gymtor1@gmail.com>
2025-06-25 20:30:07 -0400

    Update main_new.R

--- main_new.R
+++ main_new.R
@@ -36,14 +36,20 @@ print(head(unique(df_acs$city_state), 10))
 cat("\nTotal unique city-states in crime data:", length(unique(filtered_crime_data$city_state)))
 cat("\nTotal unique city-states in ACS data:", length(unique(df_acs$city_state)))
 
+#Remove one incorrect observation in Crime dataset and treat years as numeric
+filtered_crime_data <- filtered_crime_data[-1,]
+df_acs$year <- sapply(df_acs$year, as.numeric)
+filtered_crime_data$Year <- sapply(filtered_crime_data$Year, as.numeric)
+
 # First, find out how many unique years you have
 total_years <- n_distinct(filtered_crime_data$Year)
 
 # Find city-state combinations that appear in all years - Crime Data
-city_states_in_all_years_crime <- filtered_crime_data %>%
-  group_by(city_state) %>%
-  summarise(years_present = n_distinct(Year)) %>%
-  filter(years_present == total_years) %>%
+city_states_in_all_years_crime <- filtered_crime_data %>% 
+  group_by(city_state) %>% 
+  count() %>% 
+  ungroup() %>% 
+  filter(n == 9) %>% 
   pull(city_state)
 
 # Create new table with only those city-state combinations
@@ -51,10 +57,11 @@ complete_cities_data <- filtered_crime_data %>%
   filter(city_state %in% city_states_in_all_years_crime)
 
 # Find city-state combinations that appear in all years - ACS
-city_states_in_all_years_acs <- df_acs %>%
-  group_by(city_state) %>%
-  summarise(years_present = n_distinct(year)) %>%
-  filter(years_present == total_years) %>%
+city_states_in_all_years_acs <- df_acs %>% 
+  group_by(city_state) %>% 
+  count() %>% 
+  ungroup() %>% 
+  filter(n == 9) %>% 
   pull(city_state)
 
 # Create new table with only those city-state combinations
@@ -107,3 +114,57 @@ cat("ACS data rows:", nrow(filtered_acs), "\n")
 cat("Crime data rows:", nrow(filtered_crime), "\n") 
 cat("Merged data rows:", nrow(merged_data), "\n")
 cat("Missing violent crime values:", sum(is.na(merged_data$violent_crime)), "\n")
+
+grants_data <- read.csv("Data/Grants/Assistance_PrimeAwardSummaries_2025-06-25_H22M03S38_1.csv")
+
+#Create issue year
+grants_data$issue_year <- substr(grants_data$period_of_performance_start_date, 1, 4)
+
+#filter out by 2021 issue year
+grants2022 <- filter(grants_data, issue_year== 2022)
+
+#Select necessary columns and clean city and state names
+filtered_grants <- grants2021 [,c("total_funding_amount","issue_year", "recipient_city_name", "recipient_state_name","award_id_fain")]
+filtered_grants <- filtered_grants %>%
+  mutate(
+    City_clean = str_to_title(str_trim(recipient_city_name)),
+    State_clean = str_to_title(str_trim(recipient_state_name)),
+    city_state = paste(City_clean, State_clean, sep = ", "),
+    grant_type= substr(award_id_fain, 16, 19)
+  )
+
+#Filter out for only CVIPI grants (ALN code: 16.045)
+cvipi <- filtered_grants %>% 
+  filter(grant_type== "CVIP" | award_id_fain == c("15PBJA23GG05226MUMU", "15PNIJ23GG04270MUMU", "15PBJA22GG04749MUMU", "15PBJA24GG03106MUMU","15PBJA24AG00119MUMU"))
+
+#Sum the total funding for each grant
+total_cvipi <- cvipi %>% 
+  select(total_funding_amount, city_state) %>% 
+  aggregate(.~city_state, sum)
+
+total_grants <- filtered_grants %>% 
+  select(total_funding_amount, city_state) %>% 
+  aggregate(.~city_state, sum)
+
+#Filter out cities that are in both merged_data and total_cvipi and merge the two datasets
+common_grant_city_states <- intersect(
+  unique(total_cvipi$city_state), 
+  unique(merged_data$city_state)
+)
+
+grants_merged <- merged_data %>%
+  left_join(filter(total_cvipi, city_state %in% common_grant_city_states), 
+            by = c("city_state" = "city_state")) %>%
+  rename(funding2022 = total_funding_amount)
+
+#If funding2022 is NA change it to 0
+grants_merged$funding2022[is.na(grants_merged$funding2022)] <- 0
+
+#Summary Statistics!
+
+filter(grants_merged, funding2022>0) %>% 
+summary()
+         
+filter(grants_merged, funding2022>0) %>% 
+summary()
+
